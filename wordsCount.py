c6b4ffbd822ab309584ff2fed5e0b691c5b4b0f7
anu <anumathew1225@gmail.com>
2023-03-20 09:12:56 +0530

    first commit

---
+++ wordsCount.py
@@ -0,0 +1,9 @@
+# File       :wordsCount.py
+# Description:program to count the number of words in a string
+# Author     :Anu Mathew
+# Date       :20-03-2023
+
+sent=input("Enter a sentance: ")
+words=sent.split()
+print("The List of Words:",words)
+print("The Count of words: ",len(words))

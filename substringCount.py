40ee0b1dadea2674dfa937245a81a79f0fd0181d
anu <anumathew1225@gmail.com>
2023-03-20 20:19:17 +0530

    Add files to existing repo

---
+++ substringCount.py
@@ -0,0 +1,10 @@
+# File       :substringCount.py
+# Description:program to count the number of partcular substring  in a sentence
+# Author     :Anu Mathew
+# Date       :20-03-2023
+
+str=input("Enter a sentence:")
+substr=input("Enter the substring to be searched:")
+print("Count of the substring in the sentence:",str.count(substr))
+
+    

c4c6aa7171fac90eb415e2d16555b56002846f25
anu <anumathew1225@gmail.com>
2023-03-20 09:28:14 +0530

    Add files to existing repo

---
+++ upperCase.py
@@ -0,0 +1,6 @@
+# File       :wordsCount.py
+# Description:program to convert the string to uppercase
+# Author     :Anu Mathew
+# Date       :20-03-2023
+str=input("Enter a string: ")
+print("UpperCase :",str.upper())

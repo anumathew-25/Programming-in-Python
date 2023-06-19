9a05c0dfec836ee7e16225f715f8f4993956b1e1
anu <anumathew1225@gmail.com>
2023-06-19 06:05:23 +0530

    updated gui

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 root=Tk()
+root.geometry("400x250") 
 name=Label(root,text="Name").grid(row=0,column=0)
 e1=Entry(root).grid(row=0,column=1)
 password=Label(root,text="Password").grid(row=1,column=0)
